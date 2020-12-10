c7f993754eb1395f497cab67a45beef003a9da2d
FaishalFadhil <AchmadFaishal.Fadhil@outlook.com>
2020-12-10 21:31:26 +0700

    add draft question and opening socket

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 const http = require('http').createServer(app)
-const io = require('socket.io')(http);
+const io = require('socket.io')(http)
+const question = require('./question.json')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
+
+
 io.on('connection', (socket) => {
   console.log('a user connected')
+
+  socket.emit("welcomeMessage", "Hello, welcome to guess is correct or wrong! Please fill the username first") 
+  
+  
 })
 
 http.listen(PORT, () => {

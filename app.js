34def647e62a05cb3283e6cdc848f68aa6b32224
andhikam <andhikamuttaqien@gmail.com>
2020-12-11 14:10:20 +0700

    Finishing App

--- app.js
+++ app.js
@@ -14,9 +14,6 @@ app.use(express.urlencoded({extended:false}))
 
 let joinUsers = []
 let question
-let first = false
-let winner = []
-let gameOver = false
 
 io.on('connection', (socket) => {
   console.log('a user connected')
@@ -27,7 +24,9 @@ io.on('connection', (socket) => {
     joinUsers.push({
       username: payload,
       status: 'Ready',
-      score: 0
+      score: 0,
+      benar: 0,
+      salah: 0
     })
     io.emit('login', joinUsers)
   })
@@ -50,6 +49,7 @@ io.on('connection', (socket) => {
     for (let i = 0; i < joinUsers.length; i++) {
       if (joinUsers[i].username === username) {
         joinUsers[i].score -= 3
+        joinUsers[i].salah++
         break
       }
     }
@@ -60,77 +60,16 @@ io.on('connection', (socket) => {
     for (let i = 0; i < joinUsers.length; i++) {
       if (joinUsers[i].username === username) {
         joinUsers[i].score += 10
+        joinUsers[i].benar++
         if (joinUsers[i].score >= 100) {
           io.emit('moveToResult', joinUsers[i].username)
+          joinUsers = []
         }
         break
       }
     }
     io.emit('login', joinUsers)
   })
-
-  
-
-  // socket.on('result', (payload) => {  //answers
-  //   let correct = question.answer
-  //   for (let i = 0; i < payload.length; i++) {
-  //     if (payload[i].answer === correct && first === false) {
-  //       for (let j = 0; j < joinUsers.length; j++) {
-  //         if (payload[i].username === joinUsers[j].username) {
-  //           joinUsers[j].score += 8
-  //           first = true
-  //           if (joinUsers[j].score >= 50) gameOver = true
-  //           break
-  //         }
-  //       }
-  //     } else if (payload[i].answer === correct && first === true) {
-  //       for (let k = 0; k < joinUsers.length; k++) {
-  //         if (payload[i].username === joinUsers[k].username) {
-  //           joinUsers[k].score += 5
-  //           if (joinUsers[k].score >= 50) gameOver = true
-  //           break
-  //         }
-  //       }
-  //     } else {
-  //       for (let l = 0; l < joinUsers.length; l++) {
-  //         if (payload[i].username === joinUsers[l].username) {
-  //           joinUsers[l].score -= 3
-  //           break
-  //         }
-  //       }
-  //     }
-  //   }
-  //   if (gameOver) {
-  //     function compare(b, a) {
-  //       const userA = a.score
-  //       const userB = b.score
-      
-  //       let comparison = 0;
-  //       if (userA > userB) {
-  //         comparison = 1;
-  //       } else if (userA < userB) {
-  //         comparison = -1;
-  //       }
-  //       return comparison;
-  //     }
-      
-  //     joinUsers.sort(compare)
-  //     winner.push(joinUsers[0])
-  //     io.emit('gameResult', winner)
-  //     joinUsers = []
-  //     winner = []
-  //     gameOver = false
-  //   }
-  //   first = false
-  //   io.emit('result', joinUsers)
-  // })
-
-  // buat bikin room
-  // io.sockets.on('connection', function(socket) {
-  //   socket.on('create', function(room) {
-  //     socket.join(room);
-  //   });
-  // });
 })
 
 http.listen(PORT, () => {
